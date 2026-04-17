89b6a7c37c038720046cb4e12cd0bb8551b0901c
Thabo Mosikidi <mosikidi012@gmail.com>
2026-04-18 01:30:47 +0200

    fix routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controllers/auth.controller');
 
-router.post('/register', register);
+router.post('/register', (req, res) => {
+  res.json({ message: "Register works" });
+});
 router.post('/login', login);
 
 module.exports = router;
