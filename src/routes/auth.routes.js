7f7b19a22daefc88b357ec36598b77de81887502
Thabo Mosikidi <mosikidi012@gmail.com>
2026-04-18 01:47:43 +0200

    show real login error

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,13 @@ const { register, login } = require('../controllers/auth.controller');
 router.post('/register', (req, res) => {
   res.json({ message: "Register works" });
 });
-router.post('/login', login);
+router.post('/login', (req, res) => {
+  res.json({ message: "Login works" });
+});
+
+catch (err) {
+  console.error("LOGIN ERROR:", err);
+  return res.status(500).json({ error: err.message });
+};
 
 module.exports = router;
